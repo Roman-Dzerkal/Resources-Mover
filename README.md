c2fde5f1d326d6417617179e516c2612363d8481
Roman-Dzerkal <dzerkal.r@gmail.com>
2021-10-02 00:10:38 +0300

    Added readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Resources Mover
+**Special for [Unfaithful](https://vk.com/id83712522)**
+
+**author:** Roman "_mmyr_" Dzerkal 
+
+# About
+This program moves resources (f.e textures) from `From` folder to `Where` folder.
+
+List of resources locates in `.log` file in game folder.
--- src/main/java/prj/another/side/recourcesmover/HelloController.java
+++ src/main/java/prj/another/side/recourcesmover/HelloController.java
@@ -1,16 +1,43 @@
 package prj.another.side.recourcesmover;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HelloController {
+    private final Logger  CONSOLE = Logger.getLogger("HelloController");
     private static String pathFromFolder;
     private static String pathWhereFolder;
     private static String pathToLogFile;
     private static List<String> pathsList;
 
 
+    public void openFromFolder(ActionEvent actionEvent) {
+        CONSOLE.log(Level.INFO, "Open from folder\n");
+    }
 
+    public void openWhereFolder(ActionEvent actionEvent) {
+        CONSOLE.log(Level.INFO, "Open where folder\n");
+    }
+
+    public void openLogFile(ActionEvent actionEvent) {
+        CONSOLE.log(Level.INFO, "Open log file\n");
+    }
+
+    public void onAction(ActionEvent actionEvent) {
+        CONSOLE.log(Level.INFO, "On action\n");
+    }
+
+    public void onMenuValidation(Event event) {
+        CONSOLE.log(Level.INFO, "On menu validation\n");
+    }
+
+    public void startMove(ActionEvent actionEvent) {
+        CONSOLE.log(Level.INFO, "Start\n");
+    }
 }
