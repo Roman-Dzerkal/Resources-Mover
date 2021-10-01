42d1b26adace768b126669f9e746c47d04a3df3e
Roman-Dzerkal <dzerkal.r@gmail.com>
2021-10-01 23:42:28 +0300

    First commit

---
+++ src/main/java/prj/another/side/recourcesmover/HelloController.java
@@ -0,0 +1,16 @@
+package prj.another.side.recourcesmover;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.util.List;
+
+public class HelloController {
+    private static String pathFromFolder;
+    private static String pathWhereFolder;
+    private static String pathToLogFile;
+    private static List<String> pathsList;
+
+
+
+}
